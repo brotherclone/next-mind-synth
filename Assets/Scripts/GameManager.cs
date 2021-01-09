392d7dde07da11893420c2a5a355c647712c812c
Gabriel Walsh <brotherclone@gmail.com>
2021-01-09 10:44:31 -0500

    basic ui

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TMP_Text statusText;
+    
+    public List<GameObject> panels;
+    
+    // Start is called before the first frame update
+    private Animator currentAnimator;
+    void Start()
+    {
+        statusText.text = "Loaded.";
+        
+        // Initialize UI Panels
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(false); 
+            currentAnimator = panel.GetComponent<Animator>();
+            currentAnimator.SetBool("isDisplayed", false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
