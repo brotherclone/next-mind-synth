2369ccc747d17d3bd71707cc6b9628c5f5730eae
Gabriel Walsh <brotherclone@gmail.com>
2021-01-10 13:44:29 -0500

    open panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,32 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-
-public class GameManager : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class GameManager : Singleton<GameManager>
 {
-    public TMP_Text statusText;
-    
-    public List<GameObject> panels;
-    
-    // Start is called before the first frame update
-    private Animator currentAnimator;
-    void Start()
+    private string _currentSceneName = string.Empty;
+    private List<AsyncOperation> _loadOperations;
+    public GameObject[] systemPrefabs;
+    private readonly List<GameObject> _instancedSystemPrefabs;
+    public GameManager(List<GameObject> instancedSystemPrefabs)
     {
-        statusText.text = "Loaded.";
-        
-        // Initialize UI Panels
-        foreach (GameObject panel in panels)
+        _instancedSystemPrefabs = instancedSystemPrefabs;
+    }
+    private GameObject _prefabInstance;
+
+    public void LoadScene(string currentScene)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(currentScene, LoadSceneMode.Additive);
+        if (asyncOperation == null)
         {
-            panel.SetActive(false); 
-            currentAnimator = panel.GetComponent<Animator>();
-            currentAnimator.SetBool("isDisplayed", false);
+            Debug.LogError("[GameManager] Unable to load " + currentScene);
+            return;
         }
+        asyncOperation.completed += OnLoadOperationComplete;
+        _loadOperations.Add(asyncOperation);
+        _currentSceneName = currentScene;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnLoadOperationComplete(AsyncOperation asyncOperation)
     {
-        
+        if (_loadOperations.Contains(asyncOperation))
+        {
+            _loadOperations.Remove(asyncOperation);
+        }
+        Debug.Log("Scene loaded.");
+    }
+
+    void OnUnloadOperationComplete(AsyncOperation asyncOperation)
+    {
+        Debug.Log("Scene unloaded."); 
+    }
+    public void UnLoadScene(string currentScene)
+    {
+        AsyncOperation asyncOperation = SceneManager.UnloadSceneAsync(currentScene);
+        if (asyncOperation == null)
+        {
+            Debug.LogError("[GameManager] Unable to load " + currentScene);
+            return;
+        }
+        asyncOperation.completed += OnUnloadOperationComplete;
+    }
+    
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        _loadOperations = new List<AsyncOperation>();
+        LoadScene("NextMindSynth");
+        InstantiateSystemPrefabs();
+    }
+
+    private void InstantiateSystemPrefabs()
+    {
+        int i;
+        for (i = 0; i < systemPrefabs.Length; ++i)
+        {
+            _prefabInstance = Instantiate(systemPrefabs[i]);
+            _instancedSystemPrefabs.Add(_prefabInstance);
+        }
+    }
+    
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        int i;
+        for (i = _instancedSystemPrefabs.Count - 1; i >= 0; --i)
+        {
+            Destroy(_instancedSystemPrefabs[i]);
+        }
+        _instancedSystemPrefabs.Clear();
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject leftPanel;
+    public TMP_Text statusText;
+    private List<GameObject> _panels = new List<GameObject>();
+    private Animator _currentAnimator; 
+    private void InitializeUI()
+    {
+        _panels.Add(leftPanel);
+        statusText.text = "Loaded";
+        Debug.Log("UI Loaded");
+    }
+
+    public void OpenPanel()
+    { 
+        _currentAnimator = leftPanel.GetComponent<Animator>();
+        _currentAnimator.SetBool("isDisplayed", true);
+    }
+    
+    private void Start()
+    {
+        InitializeUI();
+    }
+}
+
+
+
