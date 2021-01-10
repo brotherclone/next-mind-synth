eae7cea81e4e71ea5e45bd40b8ad17fbbbab6f4b
Gabriel Walsh <brotherclone@gmail.com>
2021-01-10 17:11:57 -0500

    open panel and close

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,7 +10,8 @@ public class UIManager : MonoBehaviour
     public GameObject leftPanel;
     public TMP_Text statusText;
     private List<GameObject> _panels = new List<GameObject>();
-    private Animator _currentAnimator; 
+    private Animator _currentAnimator;
+ 
     private void InitializeUI()
     {
         _panels.Add(leftPanel);
@@ -18,17 +19,51 @@ public class UIManager : MonoBehaviour
         Debug.Log("UI Loaded");
     }
 
-    public void OpenPanel()
-    { 
-        _currentAnimator = leftPanel.GetComponent<Animator>();
-        _currentAnimator.SetBool("isDisplayed", true);
+    public void TogglePanel(PanelName panelName, TagUIInteraction _tagUIInteraction)
+    {
+        switch (panelName)
+        {
+            case PanelName.LeftPanel:
+                _currentAnimator = leftPanel.GetComponent<Animator>();
+                var status = _currentAnimator.GetBool("isDisplayed");
+                _currentAnimator.SetBool("isDisplayed", !status);
+                _tagUIInteraction.EnableDisableTag(true);
+                break;
+        }
     }
     
     private void Start()
     {
         InitializeUI();
     }
+
+    private void Update()
+    {
+        
+    }
+
+    private bool AnimationFinished()
+    {
+        return _currentAnimator.GetCurrentAnimatorStateInfo(0).length >
+               _currentAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
 }
 
+public enum UIActions{
+    Open,
+    Close
+}
 
+public enum UIType
+{
+    Panel
+}
 
+public enum PanelName
+{
+    None,
+    LeftPanel,
+    RightPanel,
+    BottomPanel,
+    MainPanel
+}
