8384e5c7760ce011c095609adb40e96fb5e8ccec
Gabriel Walsh <brotherclone@gmail.com>
2021-01-13 18:29:27 -0500

    note vals

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using JetBrains.Annotations;
+
+[Serializable]
+public class Note {
+
+    public int midiNumber;
+    public float frequency;
+    public int octaveNumber;
+    public string noteName;
+    [CanBeNull] public string noteAlias;
+
+    public string GETMidiNoteName()
+    {
+        return noteAlias+octaveNumber;
+    }
+}
