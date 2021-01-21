80f91552ee2d3f574f31931d29d0f77da3e7ea67
Gabriel Walsh <brotherclone@gmail.com>
2021-01-21 08:08:35 -0500

    octave play

--- Assets/Scripts/NoteManager.cs
+++ Assets/Scripts/NoteManager.cs
@@ -2,13 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class NoteManager : MonoBehaviour
 {
     public string noteDataPath;
     private NoteCollection _noteCollection;
-
+    public List<Note> notes;
+    public List<Note> currentScale;
+    
     private void LoadNoteData()
     {
         using (StreamReader stream = new StreamReader(noteDataPath))
@@ -17,13 +21,43 @@ public class NoteManager : MonoBehaviour
             _noteCollection = JsonUtility.FromJson<NoteCollection>(json);
             for (int i = 0; i < _noteCollection.notes.Length; ++i)
             {
-                Debug.Log(_noteCollection.notes[i].frequency);
+               notes.Add( _noteCollection.notes[i]);
             }
         }
+        MakeMajor(60);
     }
 
     private void Start()
     {
         LoadNoteData();
+    }
+
+    private void GetMidiNotesFromNumbers(IReadOnlyList<int> noteNumbers)
+    {
+        foreach (var t in noteNumbers)
+        {
+            foreach (var n in notes.Where(n => n.midi_number == t))
+            {
+                currentScale.Add(n);
+                Debug.Log(n.note_name);
+            }
+        }
+    }
+
+    private void MakeMajor(int startMidiNumber)
+    {
+        //T-T-S-T-T-T-S,
+        var majorSteps = new[] { 0,2,2,1,2,2,2,1 };
+        var majorStepCounter = 0;
+        var midiNoteCounter = startMidiNumber;
+        var scaleMidiNumbers = new List<int>();
+        while (majorStepCounter < majorSteps.Length)
+        {
+            scaleMidiNumbers.Add(midiNoteCounter);
+            if (midiNoteCounter + majorSteps[majorStepCounter] >= 127) continue;
+            midiNoteCounter += majorSteps[majorStepCounter];
+            ++majorStepCounter;
+        }
+        GetMidiNotesFromNumbers(scaleMidiNumbers);
     }
 }
---
+++ Assets/Scripts/PlayTagController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NextMind;
+using NextMind.NeuroTags;
+
+public class PlayTagController : MonoBehaviour
+{
+    public NeuroTag neuroTag;
+    
+    private float _currentConfidenceValue = 0;
+
+    private float _targetConfidenceValue = 0;
+
+    //private bool _interpolateConfidenceValue = true;
+    
+    private void Awake()
+    {
+        if (neuroTag == null)
+        {
+            neuroTag = GetComponentInParent<NeuroTag>();
+        }
+        if (neuroTag != null)
+        {
+            neuroTag.onConfidenceChanged.AddListener(OnConfidenceUpdated);
+            neuroTag.onTriggered.AddListener(OnTagTrigger);
+        }
+    }
+    
+    private void OnTagTrigger()
+    {
+        //Debug.Log("Triggered");
+    }
+    
+    private void OnConfidenceUpdated(float value)
+    {
+        //Debug.Log("Tag confidence "+ value);
+        _targetConfidenceValue = value; 
+        
+    }
+    
+}
