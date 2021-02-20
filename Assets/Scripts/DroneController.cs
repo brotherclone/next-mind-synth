963a3d0dcde9e9f59efd2fa515d498a33ee84129
Gabriel Walsh <brotherclone@gmail.com>
2021-02-20 13:49:09 -0500

    single toggle wired

---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DroneController : MonoBehaviour
+{
+    public Toggle toggle;
+
+    public void HandleToggle(Toggle toggle)
+    {
+        Debug.Log(" t -->"+ toggle.isOn);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
